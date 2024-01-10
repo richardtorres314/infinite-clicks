cd82cb6c8593c1a08aecc6044a643ceffa613cdc
Richard Torres <richardtorres314@gmail.com>
2024-01-10 19:45:54 +0100

    handling routing for different environments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/richardtorres314.github.io/infinite-clicks"
+      : undefined,
 };
 
 module.exports = nextConfig;
